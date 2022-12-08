3ef0569967917d88edb03be7f4e89300e9b865a7
Sonhar <119830153+isonhar@users.noreply.github.com>
2022-12-08 17:43:53 -0300

    _

---
+++ Poosamuel/TrabSam/src/pessoa/Pessoa.java
@@ -0,0 +1,21 @@
+package pessoa;
+
+public class Pessoa {
+	
+protected String nome;
+
+public Pessoa(String nome) {
+ this.nome=nome;
+}
+
+public String getNome() {
+	return nome;
+}
+ 
+public void setNome(String nome) {
+	this.nome = nome;
+}
+public String toString() {
+	return nome;
+ }
+}
